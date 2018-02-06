02b6ff63f6ddf0730809f160c8d44ddad259493c
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-02-06 11:43:44 +0100

    ok

--- esercizi-condizioni/la-calcolatrice.js
+++ esercizi-condizioni/la-calcolatrice.js
@@ -14,3 +14,37 @@
   Utilizza il costrutto Switch-Case mediante acquisizione di caratteri.
   http://www.imparareaprogrammare.it
 */
+var num1 = prompt('inserisci il primo numero', 1);
+var num2 = prompt('inserisci il secondo numero', 1);
+var operator = prompt('inserisci operazione', 'somma');
+
+var num1N = Number(num1);
+var num2N = Number(num2);
+
+switch(operator){
+  case 'somma':
+    console.log(num1N+num2N);
+    break;
+  case 'sottrazione':
+    console.log(num1N-num2N);
+    break;
+  case 'moltiplicazione':
+    console.log(num1N*num2N);
+    break;
+  case 'divisione':
+    console.log(num1N/num2N);
+    break;
+  case 'modulo':
+    console.log(num1N%num2N);
+    break;
+  case 'potenza':
+    console.log(Math.pow(num1N, num2N));
+    break;
+  case 'media':
+    console.log((num1N+num2N)/2);
+    break;
+    
+
+  
+
+}
