97b7ff6e6e2d1e491db675286dcaeb8bcd827119
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-02-07 11:50:44 +0100

    ok

--- esercizi-cicli/rincorri-la-decina.js
+++ esercizi-cicli/rincorri-la-decina.js
@@ -4,3 +4,95 @@
   Hint: per andare a capo stampa '\n'.
   http://www.imparareaprogrammare.it
 */
+var num = 0;
+var i;
+
+
+for (i=1; i<=10; i++) {
+    
+  num = i;
+  document.write(num);
+  
+}
+
+document.write('<br>');
+
+for (i=11; i<=20; i++) {
+    
+  num = i;
+  document.write(num);
+  
+}
+
+document.write('<br>');
+
+for (i=21; i<=30; i++) {
+    
+  num = i;
+  document.write(num);
+  
+}
+
+document.write('<br>');
+
+for (i=31; i<=40; i++) {
+    
+  num = i;
+  document.write(num);
+  
+}
+
+document.write('<br>');
+
+for (i=41; i<=50; i++) {
+    
+  num = i;
+  document.write(num);
+  
+}
+
+document.write('<br>');
+
+for (i=51; i<=60; i++) {
+    
+  num = i;
+  document.write(num);
+  
+}
+
+document.write('<br>');
+
+for (i=61; i<=70; i++) {
+    
+  num = i;
+  document.write(num);
+  
+}
+
+document.write('<br>');
+
+for (i=71; i<=80; i++) {
+    
+  num = i;
+  document.write(num);
+  
+}
+
+document.write('<br>');
+
+for (i=81; i<=90; i++) {
+    
+  num = i;
+  document.write(num);
+  
+}
+
+document.write('<br>');
+
+for (i=91; i<=100; i++) {
+    
+  num = i;
+  document.write(num);
+  
+}
+
--- esercizi-cicli/ti-ricordi-le-tabelline.js
+++ esercizi-cicli/ti-ricordi-le-tabelline.js
@@ -3,3 +3,12 @@
   Scrivi un programma che stampi che prenda in input un numero e stampi la tabellina corrispondente.
   http://www.imparareaprogrammare.it
 */
+
+var num = prompt('inserisci un numero per avere la tabellina',4);
+var i;
+
+for (i=1; i<=10; i++) {
+
+  var tab = num*i;
+  document.write(tab + '<br>');
+}
