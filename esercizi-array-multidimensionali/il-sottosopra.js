8917f3f218a1e275249bad944a9478150a8af6eb
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-02-11 20:26:49 +0100

    ok

--- esercizi-array-multidimensionali/il-sottosopra.js
+++ esercizi-array-multidimensionali/il-sottosopra.js
@@ -15,3 +15,27 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var arrInput = [
+  [1, 2],
+  [3, 4],
+  [5, 6]
+];
+
+console.log(arrInput);
+
+var x = arrInput[0][0];
+var y = arrInput[1][0];
+var z = arrInput[2][0];
+var e = arrInput[0][1];
+var j = arrInput[1][1];
+var k = arrInput[2][1];
+var a = [x, y, z];
+var b = [e, j, k];
+var arrOutput = [[a], [b]];
+
+
+console.log(a);
+console.log(b);
+console.log(arrOutput);
+
