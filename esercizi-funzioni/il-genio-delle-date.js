f9b34a597f635f3b9167e8f71aef65965c68566f
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-02-13 23:37:23 +0100

    ok

--- esercizi-funzioni/il-genio-delle-date.js
+++ esercizi-funzioni/il-genio-delle-date.js
@@ -8,3 +8,33 @@
   Hint: puoi definire un array con i giorni di ogni mese e ciclarlo.          
   http://www.imparareaprogrammare.it
 */
+
+if(typeof Array.prototype.sum !== 'function') {
+
+	Array.prototype.sum = function() {
+
+		var total = 0;
+	
+		for(var i = 0; i < this.length; i += 1) {
+	
+			total += this[i];
+	
+		}
+	
+		return total;
+
+	};
+
+}
+
+
+var arr = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+ function dateCalc(day, month) {
+   arr.splice(month);
+   var sumA = arr.sum();
+   return day + sumA;
+
+ }
+
+ console.log(dateCalc(4, 5));
+  
