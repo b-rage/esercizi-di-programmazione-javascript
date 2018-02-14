e2c27f36e888f40a891eeab3f5842089c1298e62
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-02-14 19:48:06 +0100

    ok

--- esercizi-funzioni/pa-pa-pa-lindromo.js
+++ esercizi-funzioni/pa-pa-pa-lindromo.js
@@ -7,4 +7,28 @@
             TRUE
   Hint: puoi eliminare spazi e segni di punteggiatura usando le espressioni regolarie e il metodo del tipo stringa chiamato replace, in questo modo: str.replace(/\W/g, "")
   http://www.imparareaprogrammare.it
-*/
+
+
+function f1(text) {
+    var rep = text.replace(/\W/g, "");
+    var lunghezzafrase = rep.length;
+    var media = lunghezzafrase/2;
+    if(media % 2 === 0 ) {
+      console.log('pari');
+    }else{
+      console.log('dispari');
+    }
+   console.log(media);
+  
+}*/
+
+function fastestIsPalindrome(str) {
+  var strP = str.replace(/\W/g, "");
+  var len = Math.floor(strP.length / 2);
+  for (var i = 0; i < len; i++)
+    if (strP[i] !== strP[strP.length - i - 1])
+      return false;
+  return true;
+}
+
+console.log(fastestIsPalindrome('i topi non avevano nipoti'));
