7fbf15a47170a8624cfc37b4b17b91d512ce81d1
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-02-19 21:25:54 +0100

    ok

--- esercizi-oggetti/la-mia-rubrica.js
+++ esercizi-oggetti/la-mia-rubrica.js
@@ -18,3 +18,84 @@
 */
 
 //Gestore quindi un array di oggetti prevedendo la possibileà di: inserire, modificare, visualizzare, ricercare e cancellare contatti della rubrica.
+
+var contact = {
+  nome: 'Fabio',
+  cognome: 'Rossi',
+  telefono: '654675'
+};
+
+var contactsList = [];
+
+function allContacts() {
+    for(var i=0; i<contactsList.length; i++) {
+      console.log(contactsList[i].nome);
+      
+    }
+};
+
+
+
+
+function insertContact(nomeC, cognomeC, telC) {
+  contactsList.push({
+    nome: nomeC,
+    cognome: cognomeC,
+    telefono: telC
+  });
+}
+
+
+
+function deleteContact(index) {
+  contactsList.splice(index, 1);
+}
+
+function modifyContact(index) {
+  console.log(contactsList[index]);
+   
+}
+
+function findContact(nomeC) {
+  for(var i=0; i<contactsList.length; i++)
+    if(nomeC === contactsList[i].nome) {
+      console.log(contactsList[i].nome + ' ' + contactsList[i].cognome + ' ' + contactsList[i].telefono);
+      
+    }
+}
+
+function delete2Contact(nomeC) {
+  for(var i=0; i<contactsList.length; i++)
+    if(nomeC === contactsList[i].nome) {
+      var pos = contactsList.indexOf(contactsList[i]);
+
+      contactsList.splice(pos, 1);
+      
+    }
+}
+
+function modify2Contact(nomeC) {
+  for(var i=0; i<contactsList.length; i++)
+    if(nomeC === contactsList[i].nome) {
+      var pos = contactsList.indexOf(contactsList[i]);
+
+      console.log(contactsList[i]);
+      
+      
+    }
+}
+
+insertContact('Giorgio', 'Bianchi', '675568');
+insertContact('Fabio', 'Rossi', '3455667');
+insertContact('Carlo', 'Givoni', '3455321');
+allContacts();
+//deleteContact(0);
+//modifyContact(0);
+findContact('Carlo');
+delete2Contact('Fabio');
+modify2Contact('Giorgio');
+console.log(contactsList);
+
+
+
+
