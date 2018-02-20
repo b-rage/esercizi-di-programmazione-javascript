08239ace3ae64515fd3e1689dc9b843f67721d0a
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-02-20 11:41:22 +0100

    ok

--- esercizi-oggetti/il-carcere-di-gotham-city.js
+++ esercizi-oggetti/il-carcere-di-gotham-city.js
@@ -27,3 +27,105 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var guardie = [];
+var detenuti = [];
+var fascicoli = [];
+
+var detenuto = {
+  nome: 'Giorgio',
+  cognome: 'Pacciani',
+  nascita: '13-02-1950'
+};
+
+var guardia = {
+  nome: 'Carmelo',
+  cognome: 'Sbirro',
+  nascita: '18-09-1988'
+};
+
+var fascicolo = {
+  id: 234,
+  nome: 'Giorgio',
+  cognome: 'Pacciani',
+  incarcerato: '23-03-2008',
+  scarcerazione: '23-03-2028',
+  crimine: 'omicidio',
+  evaso: true,
+  morto: false
+};
+
+function addGuardia(nomeG, cognomeG, nascitaG) {
+  return guardie.push({
+          nome: nomeG, 
+          cognome: cognomeG, 
+          nascita: nascitaG
+        });
+}
+
+function addDetenuto(nomeD, cognomeD, nascitaD) {
+  return detenuti.push({
+          nome: nomeD, 
+          cognome: cognomeD, 
+          nascita: nascitaD
+        });
+}
+
+function addFascicolo(idF, nomeF, cognomeF, incarceratoF, scarcerazioneF, crimineF, evasoF, mortoF) {
+  return fascicoli.push({
+          id: idF,
+          nome: nomeF,
+          cognome: cognomeF,
+          incarcerato: incarceratoF,
+          scarcerazione: scarcerazioneF,
+          crimine: crimineF,
+          evaso: evasoF,
+          morto: mortoF
+        });
+}
+
+function guardieList() {
+  for(var i=0; i<guardie.length; i++) {
+    console.log(guardie[i].nome + ' ' + guardie[i].cognome);
+    
+  }
+}
+
+function detenutiList() {
+  for(var i=0; i<detenuti.length; i++) {
+    console.log(detenuti[i].nome + ' ' + detenuti[i].cognome);
+    
+  }
+}
+
+function fascicoliList() {
+  for(var i=0; i<fascicoli.length; i++) {
+    console.log(fascicoli[i].id + ' ' + fascicoli[i].nome + ' ' + fascicoli[i].cognome);
+    
+  }
+}
+
+function findFascicolo(nomeFasc, cognomeFasc) {
+  for(var i=0; i<fascicoli.length; i++) {
+    if(fascicoli[i].nome == nomeFasc && fascicoli[i].cognome == cognomeFasc) {
+      console.log(fascicoli[i].id + ' ' + fascicoli[i].nome + ' ' + fascicoli[i].cognome + ' ' + fascicoli[i].incarcerato + ' ' + fascicoli[i].scarcerazione + ' ' + fascicoli[i].crimine);
+      
+    }
+  }
+}
+
+addGuardia('Carmelo', 'Sbirro', '18-09-1988');
+addGuardia('Carmelo2', 'Sbirro2', '18-09-1978');
+addGuardia('Carmelo3', 'Sbirro3', '22-09-1989');
+addDetenuto('Girgio', 'Pacciani', '13-02-1980');
+addDetenuto('Girgio2', 'Pacciani2', '13-02-1970');
+addDetenuto('Girgio3', 'Pacciani3', '13-02-1960');
+addFascicolo(234, 'Giorgio', 'Pacciani', '23-03-2008', '23-03-2028', 'omicidio', true, false);
+addFascicolo(234, 'Giorgio2', 'Pacciani2', '23-03-2000', '23-03-2025', 'omicidio', false, false);
+addFascicolo(234, 'Giorgio3', 'Pacciani3', '23-03-2010', '23-03-2020', 'rapina', false, false);
+guardieList();
+detenutiList();
+fascicoliList();
+findFascicolo('Giorgio2', 'Pacciani2');
+
+
